26b822a9005d6861564ccbee6171d0770ef8243b
siddharth <smishra1605@gmail.com>
2020-04-25 15:29:52 +0530

    HomePage UI Final

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'CoinsPage.dart';
+import 'PlayPage.dart';
 import 'Settings.dart';
 class Home extends StatefulWidget {
   @override
@@ -16,6 +17,9 @@ class _HomeState extends State<Home> {
   }
   Future navigateToSettings(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
+  }
+  Future navigateToPlay(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => PlayPage()));
   }
   @override
   Widget build(BuildContext context) {
@@ -50,6 +54,106 @@ class _HomeState extends State<Home> {
         body: Stack(
           children: <Widget>[
             Container(
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                  image: new AssetImage("assets/background.png"),
+                  fit: BoxFit.fill,),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        padding: EdgeInsets.only(top:100.0),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("Level:",style: TextStyle(fontSize: 60.0,color: Colors.white),),
+                                Text("level",style: TextStyle(fontSize: 60.0,color: Colors.red),)
+                              ],
+                            ),
+                               CircleAvatar(
+                                backgroundImage: AssetImage("assets/icon.png"),
+                                radius: 110.0,
+                              ),
+                            Container(
+                              decoration: new BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.white,
+                                    blurRadius: 5.0, // has the effect of softening the shadow
+                                    spreadRadius: 2.0, // has the effect of extending the shadow
+                                    offset: Offset(
+                                      1.0, // horizontal, move right 10
+                                      1.0, // vertical, move down 10
+                                    ),
+                                  )
+                                ],
+                              ),
+                              child: SizedBox(
+                                height: 60.0,
+                                width: 300.0,
+                                child: new RaisedButton(
+                                  elevation: 50.0,
+                                  shape: new RoundedRectangleBorder(
+                                      borderRadius: new BorderRadius.circular(12.0)),
+                                  color: Colors.lightGreen,
+                                  child: new Text("PLAY",
+                                      style: new TextStyle(fontSize: 30.0, color: Colors.white)),
+                                  onPressed:() {navigateToPlay(context);},
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            decoration: new BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueGrey,
+                                  blurRadius: 5.0, // has the effect of softening the shadow
+                                  spreadRadius: 2.0, // has the effect of extending the shadow
+                                  offset: Offset(
+                                    1.0, // horizontal, move right 10
+                                    1.0, // vertical, move down 10
+                                  ),
+                                )
+                              ],
+                            ),
+                            child: SizedBox(
+                              height: 50.0,
+                              width: 150.0,
+                              child: new RaisedButton(
+                                elevation: 50.0,
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius: new BorderRadius.circular(12.0)),
+                                color: Colors.white,
+                                child: new Text("Multiplayer",
+                                    style: new TextStyle(fontSize: 20.0, color: Colors.black45)),
+                                onPressed:() {navigateToPlay(context);},
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
---
+++ lib/Screens/PlayPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class PlayPage extends StatefulWidget {
+  @override
+  _PlayPageState createState() => _PlayPageState();
+}
+
+class _PlayPageState extends State<PlayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
