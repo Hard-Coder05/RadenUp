41600a7932319381ac506907150d86ade9cc176d
siddharth <smishra1605@gmail.com>
2020-04-26 16:35:13 +0530

    Bugs Removed

--- lib/Screens/PlayPage.dart
+++ lib/Screens/PlayPage.dart
@@ -2,10 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_future_builder/simple_future_builder.dart';
 import 'CoinsPage.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'Home.dart';
 class PlayPage extends StatefulWidget {
   @override
@@ -24,12 +24,12 @@ class _PlayPageState extends State<PlayPage> {
   initState()  {
     super.initState();
     _loadCounter();
-    this.getJsonData();
+ this.getJsonData();
   }
   Future<String> getJsonData() async{
     var response = await http.get(
-      Uri.encodeFull(url),
-      headers: {"Accept": "application/json"}
+        Uri.encodeFull(url),
+        headers: {"Accept": "application/json"}
     );
     setState(() {
       var convertDataToJson=json.decode(response.body);
@@ -45,123 +45,128 @@ class _PlayPageState extends State<PlayPage> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Level: "'$_level'.toString(),style: TextStyle(fontSize: 40.0,color: Colors.red),),
-          leading: GestureDetector(
-            onTap: () { Navigator.pop(context); },
-            child: Icon(
-              Icons.arrow_back_ios,  // add custom icons also
-            ),
-          ),
-          actions: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(right: 10.0),
-                child: GestureDetector(
-                  onTap: () {Navigator.pop(context);},
-                  child: Icon(Icons.monetization_on,color: Colors.amber,size: 40.0,),
-                )
-            ),
-            Padding(
-                padding: EdgeInsets.only(right: 20.0,top: 15.0),
-                child: GestureDetector(
-                  onTap: () {navigateToCoinsPage(context);},
-                  child: Text('$_points'.toString(),textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),),
-                )
-            ),
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: new DecorationImage(
-                  image: new AssetImage("assets/background.png"),
-                  fit: BoxFit.fill,),
+    return SimpleFutureBuilder<http.Response>(
+      future: http.get(Uri.encodeFull(url),headers: {"Accept": "application/json"}),
+      builder: (BuildContext context, http.Response response)=> Scaffold(
+          appBar: AppBar(
+            title: Text("Level: "'$_level'.toString(),style: TextStyle(fontSize: 40.0,color: Colors.red),),
+            leading: GestureDetector(
+              onTap: () { Navigator.pop(context); },
+              child: Icon(
+                Icons.arrow_back_ios,  // add custom icons also
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(top:100.0),
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Image.network(
-                                  data[_level-1]['options'][0]['image'],height: 180,width: 180,fit: BoxFit.fill,
-                                ),
-                                Image.network(
-                                  data[_level-1]['options'][1]['image'],height: 180,width: 180,fit: BoxFit.fill,
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Image.network(
-                                  data[_level-1]['options'][2]['image'],height: 180,width: 180,fit: BoxFit.fill,
-                                ),
-                                Image.network(
-                                  data[_level-1]['options'][3]['image'],height: 180,width: 180,fit: BoxFit.fill,
-                                ),
-                              ],
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                showAnswerInput(),
-                                Padding(padding: const EdgeInsets.only(top: 20.0),),
-                                Container(
-                                  decoration: new BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.white,
-                                        blurRadius: 5.0, // has the effect of softening the shadow
-                                        spreadRadius: 2.0, // has the effect of extending the shadow
-                                        offset: Offset(
-                                          1.0, // horizontal, move right 10
-                                          1.0, // vertical, move down 10
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  child: SizedBox(
-                                    height: 60.0,
-                                    width: 300.0,
-                                    child: new RaisedButton(
-                                      elevation: 50.0,
-                                      shape: new RoundedRectangleBorder(
-                                          borderRadius: new BorderRadius.circular(12.0)),
-                                      color: Colors.lightGreen,
-                                      child: new Text("SUBMIT",
-                                          style: new TextStyle(fontSize: 30.0, color: Colors.white)),
-                                      onPressed:() {
-                                        verify();
-                                      },
-                                    ),
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+            actions: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: GestureDetector(
+                    onTap: () {Navigator.pop(context);},
+                    child: Icon(Icons.monetization_on,color: Colors.amber,size: 40.0,),
+                  )
+              ),
+              Padding(
+                  padding: EdgeInsets.only(right: 20.0,top: 15.0),
+                  child: GestureDetector(
+                    onTap: () {navigateToCoinsPage(context);},
+                    child: Text('$_points'.toString(),textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),),
+                  )
+              ),
+            ],
+          ),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage("assets/background.png"),
+                    fit: BoxFit.fill,),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.only(top:20.0),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Image.network(
+                                    json.decode(response.body)['results'][_level-1]['options'][0]['image'],height: 160,width: 160,fit: BoxFit.fill,
+                                  ),
+                                  Image.network(
+                                    json.decode(response.body)['results'][_level-1]['options'][1]['image'],height: 160,width: 160,fit: BoxFit.fill,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Image.network(
+                                    json.decode(response.body)['results'][_level-1]['options'][2]['image'],height: 160,width: 160,fit: BoxFit.fill,
+                                  ),
+                                  Image.network(
+                                    json.decode(response.body)['results'][_level-1]['options'][3]['image'],height: 160,width: 160,fit: BoxFit.fill,
+                                  ),
+                                ],
+                              ),
+                              SingleChildScrollView(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    showAnswerInput(),
+                                    Padding(padding: const EdgeInsets.only(top: 20.0),),
+                                    Container(
+                                      decoration: new BoxDecoration(
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.white,
+                                            blurRadius: 5.0, // has the effect of softening the shadow
+                                            spreadRadius: 2.0, // has the effect of extending the shadow
+                                            offset: Offset(
+                                              1.0, // horizontal, move right 10
+                                              1.0, // vertical, move down 10
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      child: SizedBox(
+                                        height: 60.0,
+                                        width: 300.0,
+                                        child: new RaisedButton(
+                                          elevation: 50.0,
+                                          shape: new RoundedRectangleBorder(
+                                              borderRadius: new BorderRadius.circular(12.0)),
+                                          color: Colors.lightGreen,
+                                          child: new Text("SUBMIT",
+                                              style: new TextStyle(fontSize: 30.0, color: Colors.white)),
+                                          onPressed:() {
+                                            verify();
+                                          },
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      );
+    );
   }
   verify(){
  final x=data[_level-1]['answer'];
