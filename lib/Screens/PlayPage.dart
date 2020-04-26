4ceb4cb78487781a296fbf3e2d4406f553c88c97
siddharth <smishra1605@gmail.com>
2020-04-26 13:15:14 +0530

    Game Running Smoothly

--- lib/Screens/PlayPage.dart
+++ lib/Screens/PlayPage.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'CoinsPage.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+import 'Home.dart';
 class PlayPage extends StatefulWidget {
   @override
   _PlayPageState createState() => _PlayPageState();
@@ -13,6 +16,7 @@ class _PlayPageState extends State<PlayPage> {
   List data;
   int _points=0;
   int _level=0;
+  String _answer;
   Future navigateToCoinsPage(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => CoinsPage()));
   }
@@ -21,7 +25,6 @@ class _PlayPageState extends State<PlayPage> {
     super.initState();
     _loadCounter();
     this.getJsonData();
-
   }
   Future<String> getJsonData() async{
     var response = await http.get(
@@ -111,6 +114,42 @@ class _PlayPageState extends State<PlayPage> {
                                 ),
                               ],
                             ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                showAnswerInput(),
+                                Container(
+                                  decoration: new BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.white,
+                                        blurRadius: 5.0, // has the effect of softening the shadow
+                                        spreadRadius: 2.0, // has the effect of extending the shadow
+                                        offset: Offset(
+                                          1.0, // horizontal, move right 10
+                                          1.0, // vertical, move down 10
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  child: SizedBox(
+                                    height: 60.0,
+                                    width: 300.0,
+                                    child: new RaisedButton(
+                                      elevation: 50.0,
+                                      shape: new RoundedRectangleBorder(
+                                          borderRadius: new BorderRadius.circular(12.0)),
+                                      color: Colors.lightGreen,
+                                      child: new Text("SUBMIT",
+                                          style: new TextStyle(fontSize: 30.0, color: Colors.white)),
+                                      onPressed:() {
+                                        verify();
+                                      },
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
                           ],
                         ),
                       ),
@@ -122,5 +161,105 @@ class _PlayPageState extends State<PlayPage> {
           ],
         ),
       );
+  }
+  verify(){
+ final x=data[_level-1]['answer'];
+ _answer=answerController.text;
+ print(x);
+ print(_answer);
+ if(equalsIgnoreCase(x,_answer)){
+   _incrementCounter();
+   CorrectAnswer();
+ }
+ else{
+   return showDialog(context: context,builder: (context){
+     return AlertDialog(
+       title: Text("Tu chutiya hai"),
+     );
+   });
+ }
+  }
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final int counter = (prefs.getInt('counter') ?? 0) + 1;
+    setState(() {
+     prefs.setInt("points", _points+4);
+     prefs.setInt("levels", _level+1);
+    });
+  }
+  CorrectAnswer() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Congratulation! You gave the correct answer!'),
+          content: Text('Continue Playing?'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                );
+              },
+            ),
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PlayPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        );
+      },
+    ) ;
+  }
+  bool equalsIgnoreCase(String a, String b) =>
+      (a == null && b == null) ||
+          (a != null && b != null && a.toLowerCase() == b.toLowerCase());
+  _fieldFocusChange(BuildContext context, FocusNode currentFocus,FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+  final FocusNode _answerFocus = FocusNode();
+  final answerController = TextEditingController();
+  Widget showAnswerInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+      child: new TextFormField(
+        controller: answerController,
+        textInputAction: TextInputAction.next,
+        validator: (value) => value.isEmpty ? 'Answer can\'t be empty' : null,
+        onFieldSubmitted: (term){
+          _fieldFocusChange(context, _answerFocus, verify());
+        },
+        focusNode: _answerFocus,
+        maxLines: 1,
+        keyboardType: TextInputType.text,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Answer',
+            icon: new Icon(
+              Icons.arrow_forward,
+              color: Colors.grey,
+            )
+        ),
+        onSaved: (value) => _answer = value.trim(),
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    answerController.dispose();
+    super.dispose();
   }
 }
